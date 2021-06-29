c62ef39d8d05d81e5b581ab997e9a61d373c25bb
liuli <923035434>
2021-06-29 14:51:23 +0800

    2021-6-29

--- src/main/java/com/ll/autotransaction/util/PriceUtil.java
+++ src/main/java/com/ll/autotransaction/util/PriceUtil.java
@@ -4,15 +4,16 @@ import java.math.BigDecimal;
 
 public class PriceUtil {
 
-    public static BigDecimal getLowPrice(BigDecimal price){
-        return price.multiply(BigDecimal.valueOf(1-0.03)).setScale(2, BigDecimal.ROUND_DOWN);
+    public static BigDecimal getLowPrice(BigDecimal price) {
+//        return price.multiply(BigDecimal.valueOf(1 - 0.03)).setScale(2, BigDecimal.ROUND_DOWN);
+        return price.multiply(BigDecimal.valueOf(1 - 0.03)).setScale(2, BigDecimal.ROUND_HALF_UP);
     }
 
 
-    public static BigDecimal getHighPrice(BigDecimal price){
-        return price.multiply(BigDecimal.valueOf(1+0.031)).setScale(2, BigDecimal.ROUND_UP);
+    public static BigDecimal getHighPrice(BigDecimal price) {
+//        return price.multiply(BigDecimal.valueOf(1+0.031)).setScale(2, BigDecimal.ROUND_UP);
+        return price.multiply(BigDecimal.valueOf(1 + 0.031)).setScale(2, BigDecimal.ROUND_HALF_UP);
     }
-
 
 
 }
--- src/test/java/com/ll/autotransaction/AutoTransactionApplicationTests.java
+++ src/test/java/com/ll/autotransaction/AutoTransactionApplicationTests.java
@@ -7,9 +7,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,8 +25,19 @@ public class AutoTransactionApplicationTests {
 
     @Test
     public void demoTest(){
-        System.out.println(BigDecimal.valueOf(2.222).setScale(2, BigDecimal.ROUND_DOWN));
-        System.out.println(BigDecimal.valueOf(2.222).setScale(2, BigDecimal.ROUND_UP));
+        var str = "9:30";
+        var strArray = Arrays.asList(str.split(":"));
+        if(strArray.size()>0){
+            for (int i=0;i<strArray.size();i++){
+                var strItem = strArray.get(i);
+                if(strItem.length()<2){
+                    strItem = "0"+strItem;
+                    strArray.set(i,strItem);
+                }
+            }
+            str = strArray.stream().collect(Collectors.joining(":"));
+        }
+        System.out.println(str);
     }
 
 
